d4d3cbe73e8566f137c0947a8fdd0e8938280649
MUMU <2990275358@qq.com>
2023-01-29 14:51:54 +0800

    修改屏蔽文件

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,13 +1,14 @@
-const Service = require('../../sevice')
-// const APP_HOST = 'http://localhost:8000/'
-const APP_HOST = ' http://18bedc0b.r2.cpolar.top/'
+const Service = require('../../sevice');
+const {
+  APP_HOST
+} = require("../app/config");
 class Controller{
   async getImge (req,res){
     let {page} = req.query
     if(!page) page = 1;
     const result = await Service.getHomeImg(Number(page),'personimg');
     const total = await Service.getTotalImg('personimg');
-    res.render("home",{imgs:result,host:APP_HOST,search:'',total,curPage:page})
+    res.render("home",{imgs:result,host:APP_HOST+':8000/',search:'',total,curPage:page})
   }
   async getImgeDetails(req,res){
     const {id,title} = req.query;
@@ -17,8 +18,8 @@ class Controller{
   async searchImge(req,res){
     const {text} = req.query;
     const result = await Service.search(text,'personimg');
-    // res.json({imgs:result,host:APP_HOST});
-    res.render("search",{imgs:JSON.stringify(result),host:APP_HOST,search:text,total:result.length,curPage:1});
+    // res.json({imgs:result,host:APP_HOST+':8000/'});
+    res.render("search",{imgs:JSON.stringify(result),host:APP_HOST+':8000/',search:text,total:result.length,curPage:1});
   }
 }
 
